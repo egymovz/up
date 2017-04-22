8d5b4e6f2f2c9f73adfd4346f40deeae49932081
egymovz <ifastloadi@gmail.com>
2017-04-22 16:32:28 +0300

    Add files via upload

---
+++ effects.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){jQuery('.sblist li:nth-child(odd)').addClass('sblista');jQuery("#tabzine> ul").tabs({fx:{opacity:'toggle'}}).tabs('rotate',5000);jQuery('#newtabs> ul').tabs({fx:{opacity:'toggle'}});});
