c3643f7f85a2c6c18a82b41970f224622a306114
egymovz <ifastloadi@gmail.com>
2020-06-24 00:24:01 -0700

    bine sport

---
+++ bsport1.js
@@ -0,0 +1,23 @@
+https://www.dlxfile.com/dl/waw/wegs/singels/Dndnha.Com.Wegz.Dorak.Gai.mp3
+
+
+
+<div class="swe-youtube">
+
+<script src='https://jwpsrv.com/library/SakQCEfSEeOHhRIxOQfUww.js'></script><div id='player'>
+</div>
+<script>
+jwplayer("player").setup({file:"https://steambroadcast.akamaized.net/broadcast/76561199016471600/6669353577876871788/hls_manifest/0/steambroadcast.akamaized.net/master.m3u8?broadcast_origin=repo-02-lhr1.broadcast.steamcontent.com:80",controls:"true",width:"100%",height:"100%",stretching:"uniform",autostart:"true",primary:"flash",skin:"glow",androidhls:"true"});
+</script>
+<style>.jw-slider-time.jw-background-color.jw-reset.jw-slider-horizontal.jw-reset,.jw-icon.jw-icon-inline.jw-button-color.jw-reset.jw-icon-rewind,.jwplayer.jw-flag-rightclick-open .jw-rightclick,.jw-text-elapsed,.jw-time-tip::after, .jw-controlbar .jw-tooltip::after, .jw-settings-menu .jw-tooltip::after{display:none}.jw-text-live.jw-dvr-live{box-shadow:initial}.jw-text-live::before{height:6px;width:6px;background-color:#f00}.jw-text-live.jw-dvr-live::before{background-color:#757575}</style>
+
+
+
+<div class='twitter-widgett'>
+egymovz.blogspot.com.com | ايجي موفيز
+<br />
+</div>
+<style type='text/css'>
+.twitter-widgett{z-index: 90; top: 90%; position: fixed; width: 100%; height: 0%; text-align: center; font-weight: bold; color: #fff; font-size: 14px;}
+</style>
+<span id="goog_1837429471"></span><span id="goog_1837429472"></span><br /></div>
